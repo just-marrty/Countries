80b2917a2cb7418601e873465193f7bad70e01cf
Martin <marrty.h.1992@gmail.com>
2025-11-19 19:16:05 +0100

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Martin Hrbáček on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
